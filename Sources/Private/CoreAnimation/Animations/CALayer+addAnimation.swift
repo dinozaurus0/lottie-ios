6f20a4ffbe6506cae1d03d976c8777a69c562aa8
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2026-08-03 18:16:25 +0300

    Solved crash generated by modifying layers outside the main thread.

--- Sources/Private/CoreAnimation/Animations/CALayer+addAnimation.swift
+++ Sources/Private/CoreAnimation/Animations/CALayer+addAnimation.swift
@@ -186,7 +186,7 @@ extension CALayer {
       // then we can apply the keyframe value directly to the layer using KVC instead
       // of creating a `CAAnimation`.
       let currentValue = value(forKey: property.caLayerKeypath) as? ValueRepresentation
-      if property.isDefaultValue(currentValue) {
+      if property.isDefaultValue(currentValue), !CALayer.usesBackgroundThread {
         setValue(keyframeValue, forKeyPath: property.caLayerKeypath)
         return nil
       }
